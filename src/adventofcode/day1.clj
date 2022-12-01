4edca46be0e08680905e38c6cc10f4ef4b1f594a
eliseev.leonid <eliseev@arrival.com>
2022-12-01 18:36:59 +0300

    Init

---
+++ src/adventofcode/day1.clj
@@ -0,0 +1,38 @@
+(ns adventofcode.day1
+  (:require
+    [clojure.string :as string]))
+
+(defn parse-input
+  [input]
+  (->> input
+    (string/split-lines)
+    (mapv parse-long))
+  )
+
+(defn sort-by-asc
+  [input]
+  (->> input
+    (partition-by #(nil? %))
+    (mapv #(apply + %))
+    (remove nil?)
+    (sort >)))
+
+
+(defn -main
+  [& args]
+  )
+
+(comment
+  ;; part 1
+  (->> (slurp "resources/day1.in")
+    (parse-input)
+    (sort-by-asc)
+    (first))
+  
+  ;; part 2
+  (->> (slurp "resources/day1.in")
+    (parse-input)
+    (sort-by-asc)
+    (take 3)
+    (reduce +)))
+  
